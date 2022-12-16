bfc9740dd501a034f0ea6be0762c0b53195875e0
adnen rebai <adnen.rebai@gmail.com>
2022-12-16 15:52:30 +0100

    update to es module

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,19 @@
 // rollup.config.js
-import pkg from './package.json' assert { type: "json" };
+
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 const extensions = ['.js', '.ts']
 
 const config = {
   input: "src/index.js",
-  output:[
-    {
-        file: pkg.main,
-        format: 'cjs'
-     },
-    {
-        file: pkg.module,
-        format: 'es'
-    }
-],
+  output: {
+    format: 'es',
+    dir: './dist/',
+    name : 'useStorage' ,
+    globals: {
+      'crypto-js': 'CryptoJS',
+    },
+  },
 
   plugins: [
    
